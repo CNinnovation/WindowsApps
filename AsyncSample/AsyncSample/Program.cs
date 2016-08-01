a5c8e3a62867a00584a68036160f0cee8e038eeb
Christian Nagel <christian@christiannagel.com>
2016-08-01 16:40:36 +0200

    multiple samples from Monday

---
+++ AsyncSample/AsyncSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            Program p = new Program();
+            p.CallGreeting().Wait();
+        }
+
+        public async Task CallGreeting()
+        {
+            Console.WriteLine($"{nameof(CallGreeting)} started");
+            //string result = Greeting(1000, "Stephanie");
+            //Console.WriteLine(result);
+
+            Task<string> t1 = GreetingAsync(1000, "Stephanie");
+            Task<string> t2 = GreetingAsync(600, "Matthias");
+            await Task.WhenAll(t1, t2);
+            Console.WriteLine(t1.Result);
+            Console.WriteLine(t2.Result);
+            Console.WriteLine($"{nameof(CallGreeting)} ended");
+        }
+
+        public Task<string> GreetingAsync(int ms, string name)
+        {
+            return Task.Run<string>(() =>
+            {
+                return Greeting(ms, name);
+            });
+        }
+
+        public string Greeting(int ms, string name)
+        {
+            Thread.Sleep(ms);
+            return $"Hello, {name}";
+        }
+    }
+}
---
+++ BooksSample/BooksSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BooksSample.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BooksSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var service = new BooksService();
+            // Book b = service.GetTheBook();
+            // button2.Content = service.GetBooks();
+            list1.ItemsSource = service.GetBooks();
+            flip1.ItemsSource = service.GetBooks();
+        }
+    }
+}
