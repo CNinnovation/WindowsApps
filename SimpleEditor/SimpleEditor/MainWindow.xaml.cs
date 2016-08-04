59211867ebf743a6adcaac7cba57af0a3ba904c4
Christian Nagel <christian@christiannagel.com>
2016-08-04 17:35:00 +0200

    add simple editor

---
+++ SimpleEditor/SimpleEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClose(object sender, ExecutedRoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void OnOpen(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                string filename = dlg.FileName;
+                text1.Text = File.ReadAllText(filename);
+            }
+        }
+    }
+}
